070699d2fe324ecccf5a946cfe30b431dc546e9f
Bosco Choi <bchoi@lcs.on.ca>
2020-12-05 11:02:53 -0500

    A structure to post jokes

---
+++ JokeEmojiApp/JokeStructure.swift
@@ -0,0 +1,14 @@
+//
+//  JokeStructure.swift
+//  JokeEmojiApp
+//
+//  Created by Chun Choi on 2020-12-05.
+//
+
+struct Joke : Codable{
+    // Api endpiont at https://official-joke-api.appspot.com/random_joke
+    var id:Int
+    var type:String
+    var setup:String
+    var punchline:String
+}
