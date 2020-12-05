5bf3ecbf657154a57a281f3d3385555fcb11797e
Bosco Choi <bchoi@lcs.on.ca>
2020-12-05 11:32:01 -0500

    Found an way to make random emoji

---
+++ JokeEmojiApp/RandomEmoji.swift
@@ -0,0 +1,31 @@
+//
+//  RandomEmoji.swift
+//  JokeEmojiApp
+//
+//  Created by Chun Choi on 2020-12-05.
+//
+
+import SwiftUI
+
+extension String{ // Getting random Emoji in swift code from https://gist.github.com/iandundas/59303ab6fd443b5eec39
+    static func randomEmoji()->String{
+        let range = [UInt32](0x1F601...0x1F64F)
+        let ascii = range[Int(drand48() * (Double(range.count)))]
+        let emoji = UnicodeScalar(ascii)?.description
+        return emoji!
+    }
+}
+struct RandomEmoji: View {
+    var body: some View {
+        VStack{
+            Text(String.randomEmoji())
+            .navigationBarTitle("Random Emoji")
+        }
+    }
+}
+
+struct RandomEmoji_Previews: PreviewProvider {
+    static var previews: some View {
+        RandomEmoji()
+    }
+}
