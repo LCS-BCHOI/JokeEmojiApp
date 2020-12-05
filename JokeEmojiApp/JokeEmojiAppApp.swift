f9ab004268985d25b0bb70544b6dc6c21243682b
Bosco Choi <bchoi@lcs.on.ca>
2020-12-05 10:51:36 -0500

    Initial Commit

---
+++ JokeEmojiApp/JokeEmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeEmojiAppApp.swift
+//  JokeEmojiApp
+//
+//  Created by Chun Choi on 2020-12-05.
+//
+
+import SwiftUI
+
+@main
+struct JokeEmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
