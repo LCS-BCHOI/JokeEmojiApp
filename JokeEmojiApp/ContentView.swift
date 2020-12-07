d79538d5016c5381421a54bce2711f2a0895ca79
Bosco Choi <bchoi@lcs.on.ca>
2020-12-07 16:18:41 -0500

    added navigation link to other places

--- JokeEmojiApp/ContentView.swift
+++ JokeEmojiApp/ContentView.swift
@@ -23,6 +23,11 @@ struct ContentView: View {
                     Text("Get A Joke")
                 }
                 Spacer()
+                NavigationLink(destination: SavedJokeView()){
+                    Text("All Saved Joke")
+                }
+                
+                Spacer()
                 
             }
             .navigationBarTitle("Jokes And Emoji")
